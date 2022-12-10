1386c6ea9d88686fe71a0bfe1468cd4b4209a344
Jash <jashgarala19@gmail.com>
2022-12-10 10:48:54 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8L-7Vglz7S-lk3A6qxJn6Axz-gmUdkvw",
+  authDomain: "cryptogeek-98e4b.firebaseapp.com",
+  projectId: "cryptogeek-98e4b",
+  storageBucket: "cryptogeek-98e4b.appspot.com",
+  messagingSenderId: "850707655059",
+  appId: "1:850707655059:web:552438cce76e1d1a882066"
+};
+export default firebaseConfig;
